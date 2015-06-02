0b787b32e65eeb8c94c3e6f958ec4fd24742bedc
Dylan Lacey <github@dylanlacey.com>
2015-06-02 00:25:31 -0700

    Added narrow design

--- spec/first_spec.rb
+++ spec/first_spec.rb
@@ -5,4 +5,8 @@ describe "The face of the moon", :type => :feature do
     visit "http://www.google.com"
     combobox = find :id, "lst-ib"
   end
+
+  it "visits narrow design" do
+    visit "http://www.narrowdesign.com/"
+  end
 end
